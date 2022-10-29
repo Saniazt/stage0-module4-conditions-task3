808ff2ca4b2535114b61f1a6da1c3cf8a2e9df03
saniazt <saniazt@gmail.com>
2022-10-29 22:42:21 +0300

    1

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character < 'A' || character > 'z') System.out.println("wrong alphabet!");
+        else if (character == 'A' || character == 'a' || character == 'E' || character == 'e' ||
+                character == 'I' || character == 'i' || character == 'O' || character == 'o' ||
+                character == 'U' || character == 'u' || character == 'Y' || character == 'y')
+            System.out.println("Vowel");
+        else System.out.println("Consonant");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double salaryAfterTaxis;
+        if (salary <= 10000 && salary >= 0) {
+            salaryAfterTaxis = salary * 0.85;
+            System.out.println(salaryAfterTaxis);
+        } else if (salary > 10000 && salary <= 20000) {
+            salaryAfterTaxis = salary * 0.82;
+            System.out.println(salaryAfterTaxis);
+        } else if (salary > 20000) {
+            salaryAfterTaxis = salary * 0.8;
+            System.out.println(salaryAfterTaxis);
+        } else System.out.println("wrong input!");
     }
 }
